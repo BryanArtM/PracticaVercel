d52a49479749b9bbad267859427879475fec4198
BryanArtM <barteagame@gmail.com>
2024-11-04 10:35:11 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La importancia de la ética en el desarrollo de software",
+"autor" => "Fernando López",
+"resumen" => "En un mundo cada vez más digital, es crucial que los desarrolladores de software consideren la ética en sus proyectos. La responsabilidad social debe ser una prioridad en la creación de tecnologías que impactan la vida de millones.",
 ];
 ?>
