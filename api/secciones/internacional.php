65ac758255bd82bc2de43bd8db7978e37e08d4c5
BryanArtM <barteagame@gmail.com>
2024-11-04 10:32:03 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La Unión Europea propone nuevas regulaciones para la inteligencia artificial",
+"autor" => "Elena Ruiz",
+"resumen" => "La Comisión Europea presenta un proyecto de ley que busca regular el uso de la inteligencia artificial en el desarrollo de software, con el objetivo de garantizar la ética y la transparencia en su implementación en toda la región.",
 ];
 ?>
