27308e691240808521e34d187e663727b72d927a
BryanArtM <barteagame@gmail.com>
2024-11-04 10:31:24 -0500

    Update git.php

--- api/secciones/git.php
+++ api/secciones/git.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "GitHub introduce nuevas funciones para mejorar la colaboración en proyectos",
+"autor" => "Javier Sánchez",
+"resumen" => "GitHub lanza herramientas avanzadas de colaboración, incluyendo revisiones de código más eficientes y mejoras en la integración continua, para facilitar el trabajo en equipo en el desarrollo de software.",
 ];
 ?>
