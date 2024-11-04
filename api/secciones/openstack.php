f875b477588be9148a7bee4f83ca68b29e91482a
BryanArtM <barteagame@gmail.com>
2024-11-04 10:34:29 -0500

    Update openstack.php

--- api/secciones/openstack.php
+++ api/secciones/openstack.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openstack = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "OpenStack Summit 2024 se centrará en la sostenibilidad y la innovación en la nube",
+"autor" => "Laura García",
+"resumen" => "El próximo OpenStack Summit reunirá a expertos y desarrolladores de todo el mundo para discutir cómo las tecnologías de nube abierta pueden contribuir a la sostenibilidad y a la innovación en la industria tecnológica.",
 ];
 ?>
