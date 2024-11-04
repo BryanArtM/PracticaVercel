706c9d88dfd9728c9e10cf780177a5d8f34104d9
BryanArtM <barteagame@gmail.com>
2024-11-04 10:29:41 -0500

    Update empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La demanda de desarrolladores de software alcanza niveles récord en 2024",
+"autor" => "Ana Martínez",
+"resumen" => "Las empresas de tecnología y startups buscan activamente talento en desarrollo de software, con un aumento del 25% en ofertas de empleo, impulsado por la expansión de proyectos de inteligencia artificial y transformación digital.",
 ];
 ?>
