cd454723c2bc59ded9374f2719f3065a6902de62
BryanArtM <barteagame@gmail.com>
2024-11-04 10:35:58 -0500

    Update sociedad.php

--- api/secciones/sociedad.php
+++ api/secciones/sociedad.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La tecnología como herramienta para la inclusión social",
+"autor" => "Clara Ruiz",
+"resumen" => "Iniciativas de software están cambiando la vida de comunidades marginadas al facilitar el acceso a educación y recursos, demostrando cómo la tecnología puede ser un motor de inclusión y desarrollo social.",
 ];
 ?>
