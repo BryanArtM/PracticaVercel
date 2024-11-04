2ccbdaeee51af70cb91a31758729ea49e3bd4749
BryanArtM <barteagame@gmail.com>
2024-11-04 10:28:57 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "El mercado de software empresarial crecerá un 15% en 2024",
+"autor" => "Laura González",
+"resumen" => "La demanda de soluciones de software empresarial sigue en aumento, impulsada por la digitalización de procesos y el auge de la inteligencia artificial en los sectores financieros y de manufactura.",
 ];
 ?>
