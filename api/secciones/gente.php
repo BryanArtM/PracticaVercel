d9c34bde52e8c573e921bd42171dc1cf4b103d8b
BryanArtM <barteagame@gmail.com>
2024-11-04 10:30:35 -0500

    Update gente.php

--- api/secciones/gente.php
+++ api/secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "El auge de las mujeres en tecnología: un cambio positivo en el desarrollo de software",
+"autor" => "María Torres",
+"resumen" => "Con iniciativas como Women Who Code y Girls Who Code, más mujeres están ingresando en el campo del desarrollo de software, lo que promete diversificar y enriquecer la industria tecnológica.",
 ];
 ?>
