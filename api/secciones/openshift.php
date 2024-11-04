ec690346abf88e654295961cef8bfe72f1013e0e
BryanArtM <barteagame@gmail.com>
2024-11-04 10:33:48 -0500

    Update openshift.php

--- api/secciones/openshift.php
+++ api/secciones/openshift.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openshift = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "OpenShift 4.12 lanza nuevas características para mejorar la escalabilidad",
+"autor" => "Diego Ramírez",
+"resumen" => "Red Hat presenta la última versión de OpenShift, que incluye mejoras en la gestión de contenedores y una integración más fluida con Kubernetes, facilitando el escalado de aplicaciones en entornos de producción.",
 ];
 ?>
