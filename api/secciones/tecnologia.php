50aadba4af8c6619b6bb631e7665573a7d167e5e
BryanArtM <barteagame@gmail.com>
2024-11-04 10:37:05 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La llegada de la computación cuántica transforma el desarrollo de software",
+"autor" => "Miguel Ángel Pérez",
+"resumen" => "Con el avance de la computación cuántica, los desarrolladores deben adaptarse a nuevas paradigmas de programación, lo que promete revolucionar la resolución de problemas complejos en diversas industrias.",
 ];
 ?>
