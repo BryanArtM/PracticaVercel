f45e71f30287c8f340a8b8fa2997435cf8717ded
BryanArtM <barteagame@gmail.com>
2024-11-04 10:27:27 -0500

    Update cultura.php

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "El regreso de la Feria del Libro en Madrid",
+"autor" => "María González",
+"resumen" => "La Feria del Libro de Madrid regresa con más de 300 editoriales y actividades literarias que buscan fomentar la lectura entre los más jóvenes.",
 ];
 ?>
