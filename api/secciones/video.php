7de9a7482a000aa1065eaed58a409ed34b1da696
BryanArtM <barteagame@gmail.com>
2024-11-04 10:38:16 -0500

    Update video.php

--- api/secciones/video.php
+++ api/secciones/video.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $video = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Nuevas herramientas de edición de video basadas en inteligencia artificial",
+"autor" => "José Martínez",
+"resumen" => "Las últimas innovaciones en software de edición de video están utilizando inteligencia artificial para automatizar tareas, permitiendo a los creadores de contenido trabajar de manera más eficiente y creativa.",
 ];
 ?>
