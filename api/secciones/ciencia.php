351986dd3fc4bff946ec8766c03d60e851254293
BryanArtM <barteagame@gmail.com>
2024-11-04 10:21:48 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Descubren un nuevo planeta habitable en la zona Goldilocks",
+"autor" => "Dr. Ana López",
+"resumen" => "Un equipo de astrónomos ha identificado un nuevo exoplaneta que se encuentra en la zona habitable de su estrella, donde las condiciones podrían ser adecuadas para la vida tal como la conocemos. Este descubrimiento ofrece nuevas oportunidades para la búsqueda de vida extraterrestre.",
 ];
 ?>
