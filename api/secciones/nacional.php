b6e7c8c29e2755e4b4c4058314023084557fdd77
BryanArtM <barteagame@gmail.com>
2024-11-04 10:33:10 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Iniciativas locales impulsan el desarrollo de software en comunidades rurales",
+"autor" => "Sofía Martínez",
+"resumen" => "Varias organizaciones en el país están lanzando programas de formación en desarrollo de software en comunidades rurales, con el fin de promover la inclusión digital y fomentar el emprendimiento tecnológico.",
 ];
 ?>
