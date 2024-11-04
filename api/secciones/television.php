d4b434c48db55326a41173edc906eb4dfcfadcd4
BryanArtM <barteagame@gmail.com>
2024-11-04 10:37:43 -0500

    Update television.php

--- api/secciones/television.php
+++ api/secciones/television.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $television = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "El impacto del streaming en la producción de series y películas",
+"autor" => "Ana Beltrán",
+"resumen" => "Las plataformas de streaming están cambiando la forma en que se producen y distribuyen las series y películas, impulsando la innovación en el desarrollo de software para mejorar la experiencia del usuario.",
 ];
 ?>
