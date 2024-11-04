4a09cfeb6703e2bd2694460dc4c130e68e5b5ecf
BryanArtM <barteagame@gmail.com>
2024-11-04 10:23:49 -0500

    Update contenedores.php

--- api/secciones/contenedores.php
+++ api/secciones/contenedores.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La llegada de Python 3.11: mejoras en velocidad y gestión de errores",
+"autor" => "Juan Pérez",
+"resumen" => "La nueva versión de Python, 3.11, ha sido lanzada con importantes mejoras en el rendimiento, con una ejecución de hasta un 30% más rápida. Además, incluye nuevas características que facilitan la detección de errores, lo que promete mejorar la experiencia de desarrollo.",
 ];
 ?>
